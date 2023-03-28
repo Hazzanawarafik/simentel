f7e5e7b948c1eedb7cb06f96959f5b49daf336d1
Hazzanawarafik <hazzanawa@gmail.com>
2023-03-28 14:40:23 +0700

    backend pendaftaran

---
+++ modules/pemohon/language/english/web_lang.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['pemohon'] = 'Pemohon';
+$lang['pemohon_id'] = 'Pemohon Id';
+$lang['pemohon_nama'] = 'NamaPemohon';
+$lang['pemohon_jenkel'] = 'Jenis Kelamin';
+$lang['pemohon_alamat'] = 'Alamat Pemohon';
+$lang['pemohon_rt'] = 'RT';
+$lang['pemohon_rw'] = 'RW';
+$lang['kecamatan_id'] = 'Kecamatan';
+$lang['kelurahan_id'] = 'Kelurahan';
+$lang['agama_id'] = 'Agama';
+$lang['pemohon_kode'] = 'Pemohon Kode';
--- modules/pendaftaran/controllers/Pendaftaran.php
+++ modules/pendaftaran/controllers/Pendaftaran.php
@@ -17,39 +17,24 @@ class Pendaftaran extends Front {
         $this->template->build('pendaftaran');
     }
 
-    // public function add(){
-    //     $nomor_surat = $this->input->post('nomor_surat');
-    //     // $nama_perusahaan = $this->post('nama_perusahaan');
-
-
-    //     $data = array(
-    //         'nomor_surat'        => $nomor_surat,
-    //         // 'nama_perusahaan' => $nama_perusahaan,
-    //     );
-
-    //     // $this->db->insert('pendaftaran',$data);
-    //     $this->m_pendaftaran->Add($data);
-    //     redirect('peta-menara');
-    // }
-
     public function add(){
         $this->form_validation->set_rules('nomor_surat', 'Nomor Surat', 'required');
-        // $this->form_validation->set_rules('tempat', 'Tempat', 'required');
-        // $this->form_validation->set_rules('tanggal_surat', 'Tanggal Surat', 'required');
-        // $this->form_validation->set_rules('nama_perusahaan', 'Nama Perusahaan', 'required');
-        // $this->form_validation->set_rules('alamat_perusahaan', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('nama_penanggungjawab', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('no_telepon_kantor', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('jabatan', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('no_hp', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('nama_pemilik', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('alamat_pemilik', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('lokasi_tanah', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('luas_tanah', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('no_persil_tanah', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('tanah_dipakai', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('latitude', 'fieldlabel', 'required');
-        // $this->form_validation->set_rules('longitude', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('tempat', 'Tempat', 'required');
+        $this->form_validation->set_rules('tanggal_surat', 'Tanggal Surat', 'required');
+        $this->form_validation->set_rules('nama_perusahaan', 'Nama Perusahaan', 'required');
+        $this->form_validation->set_rules('alamat_perusahaan', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('nama_penanggungjawab', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('no_telepon_kantor', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('jabatan', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('no_hp', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('nama_pemilik', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('alamat_pemilik', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('lokasi_tanah', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('luas_tanah', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('no_persil_tanah', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('tanah_dipakai', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('latitude', 'fieldlabel', 'required');
+        $this->form_validation->set_rules('longitude', 'fieldlabel', 'required');
 
         // $this->form_validation->set_rules('ktp_pemohon', 'fieldlabel', 'trim|required|min_length[5]|max_length[12]');
         // $this->form_validation->set_rules('akte_perusahaan', 'fieldlabel', 'trim|required|min_length[5]|max_length[12]');
@@ -59,99 +44,176 @@ class Pendaftaran extends Front {
         // $this->form_validation->set_rules('sp_fo_ducting', 'fieldlabel', 'trim|required|min_length[5]|max_length[12]');
         // $this->form_validation->set_rules('gambar_site', 'fieldlabel', 'trim|required|min_length[5]|max_length[12]');
         // $this->form_validation->set_rules('sp_menara_bersama', 'fieldlabel', 'trim|required|min_length[5]|max_length[12]');
+
         
-        if($this->form_validation->run() == FALSE){
-            // Jika tidak lolos validasi form 
-            // $this->template->build('pendaftaran');
+        if ($this->form_validation->run() == FALSE) {
+            // Jika tidak lolos validasi form
             redirect('pendaftaran','refresh');
-        }else{
-            $config['upload_path']          = './uploads/pendaftaran';
-            $config['allowed_types']        = 'gif|jpg|png|jpeg';
-            $config['max_size']             = 2048;
-             // Load library upload
-            $this->load->library('upload', $config);
-            $this->upload->initialize($config);
-            $field_name = "ktp_pemohon";
+            
+        } else {
+            // Jika lolos validasi form
+            $ktp_pemohon = 'ktp_pemohon'; 
+            $akte_perusahaan = 'akte_perusahaan'; 
+            $surat_perjanjian = 'surat_perjanjian'; 
+            $jumlah_titik_menara = 'jumlah_titik_menara'; 
+            $kontruksi_menara = 'kontruksi_menara'; 
+            $sp_fo_ducting = 'sp_fo_ducting'; 
+            $gambar_site = 'gambar_site'; 
+            $sp_menara_bersama = 'sp_menara_bersama'; 
 
-            // $this->load->library('upload', $config);
+            if($ktp_pemohon = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
 
-            if ( ! $this->upload->do_upload('field_name'))
-            {
-                //Jika gagal upload gambar
-                
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('ktp_pemohon')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $ktp_pemohon= $this->upload->data();
+                    $ktp_pemohon= $ktp_pemohon['file_name'];
+                }
             }
-            else
-            {
-                // Jika berhasil upload gambar
-                    $data = array('uploads' => $this->upload->data());
-                    $config['image_library'] = 'gd2';
-                    $config['source_image'] = './uploads/pendaftaran/'.$upload_data['uploads']['file_name'];
-                    $this->load->library('image_lib'.$config);
-                    $data = array(
-                        'nomor_surat' => $this->input->post('nomor_surat'),
-                        // 'tempat' => $this->input->post('tempat'),
-                        // 'tanggal_surat' => $this->input->post('tanggal_surat'),
-                        // 'nama_perusahaan' => $this->input->post('nama_perusahaan'),
-                        // 'alamat_perusahaan' => $this->input->post('alamat_perusahaan'),
-                        // 'nama_penanggungjawab' => $this->input->post('nama_penanggungjawab'),
-                        // 'no_telepon_kantor' => $this->input->post('no_telepon_kantor'),
-                        // 'jabatan' => $this->input->post('jabatan'),
-                        // 'no_hp' => $this->input->post('no_hp'),
-                        // 'nama_pemilik' => $this->input->post('nama_pemilik'),
-                        // 'alamat_pemilik' => $this->input->post('alamat_pemilik'),
-                        // 'lokasi_tanah' => $this->input->post('lokasi_tanah'),
-                        // 'luas_tanah' => $this->input->post('luas_tanah'),
-                        // 'no_persil_tanah' => $this->input->post('no_persil_tanah'),
-                        // 'tanah_dipakai' => $this->input->post('tanah_dipakai'),
-                        // 'latitude' => $this->input->post('latitude'),
-                        // 'longitude' => $this->input->post('longitude'),
-                        'ktp_pemohon' => $upload_data['uploads']['file_name'],
-        
-                        // 'ktp_pemohon' => $this->input-post('ktp_pemohon'),
-                        // 'akte_perusahaan' => $this->input-post('akte_perusahaan'),
-                        // 'surat_perjanjian' => $this->input-post('surat_perjanjian'),
-                        // 'jumlah_titik_menara' => $this->input-post('jumlah_titik_menara'),
-                        // 'kontruksi_menara' => $this->input-post('kontruksi_menara'),
-                        // 'sp_fo_ducting' => $this->input-post('sp_fo_ducting'),
-                        // 'gambar_site' => $this->input-post('gambar_site'),
-                        // 'sp_menara_bersama' => $this->input-post('sp_menara_bersama'),
-                    );
-                    $this->m_pendaftaran->Add($data);
-                    redirect('peta-menara','refresh');
+
+            if($akte_perusahaan = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('akte_perusahaan')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $akte_perusahaan= $this->upload->data();
+                    $akte_perusahaan= $akte_perusahaan['file_name'];
+                }
             }
+
+            if($surat_perjanjian = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('surat_perjanjian')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $surat_perjanjian= $this->upload->data();
+                    $surat_perjanjian= $surat_perjanjian['file_name'];
+                }
+            }
+
+            if($jumlah_titik_menara = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('jumlah_titik_menara')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $jumlah_titik_menara= $this->upload->data();
+                    $jumlah_titik_menara= $jumlah_titik_menara['file_name'];
+                }
+            }
+
+            if($kontruksi_menara = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('kontruksi_menara')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $kontruksi_menara= $this->upload->data();
+                    $kontruksi_menara= $kontruksi_menara['file_name'];
+                }
+            }
+
+            if($sp_fo_ducting = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('sp_fo_ducting')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $sp_fo_ducting= $this->upload->data();
+                    $sp_fo_ducting= $sp_fo_ducting['file_name'];
+                }
+            }
+
+            if($gambar_site = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('gambar_site')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $gambar_site= $this->upload->data();
+                    $gambar_site= $gambar_site['file_name'];
+                }
+            }
+
+            if($sp_menara_bersama = ''){}else{
+                $config['upload_path']      = './uploads/pendaftaran';
+                $config['allowed_types']    = 'jpg|jpeg|png|gif|pdf';
+                $config['max_size']         = 5000;
+
+                $this->load->library('upload',$config);
+
+                if(!$this->upload->do_upload('sp_menara_bersama')){
+                    //echo "Gambar gagal di upload";
+                }else{
+                    $sp_menara_bersama= $this->upload->data();
+                    $sp_menara_bersama= $sp_menara_bersama['file_name'];
+                }
+            }
+
             $data = array(
                 'nomor_surat' => $this->input->post('nomor_surat'),
-                // 'tempat' => $this->input->post('tempat'),
-                // 'tanggal_surat' => $this->input->post('tanggal_surat'),
-                // 'nama_perusahaan' => $this->input->post('nama_perusahaan'),
-                // 'alamat_perusahaan' => $this->input->post('alamat_perusahaan'),
-                // 'nama_penanggungjawab' => $this->input->post('nama_penanggungjawab'),
-                // 'no_telepon_kantor' => $this->input->post('no_telepon_kantor'),
-                // 'jabatan' => $this->input->post('jabatan'),
-                // 'no_hp' => $this->input->post('no_hp'),
-                // 'nama_pemilik' => $this->input->post('nama_pemilik'),
-                // 'alamat_pemilik' => $this->input->post('alamat_pemilik'),
-                // 'lokasi_tanah' => $this->input->post('lokasi_tanah'),
-                // 'luas_tanah' => $this->input->post('luas_tanah'),
-                // 'no_persil_tanah' => $this->input->post('no_persil_tanah'),
-                // 'tanah_dipakai' => $this->input->post('tanah_dipakai'),
-                // 'latitude' => $this->input->post('latitude'),
-                // 'longitude' => $this->input->post('longitude'),
+                'tempat' => $this->input->post('tempat'),
+                'tanggal_surat' => $this->input->post('tanggal_surat'),
+                'nama_perusahaan' => $this->input->post('nama_perusahaan'),
+                'alamat_perusahaan' => $this->input->post('alamat_perusahaan'),
+                'nama_penanggungjawab' => $this->input->post('nama_penanggungjawab'),
+                'no_telepon_kantor' => $this->input->post('no_telepon_kantor'),
+                'jabatan' => $this->input->post('jabatan'),
+                'no_hp' => $this->input->post('no_hp'),
+                'nama_pemilik' => $this->input->post('nama_pemilik'),
+                'alamat_pemilik' => $this->input->post('alamat_pemilik'),
+                'lokasi_tanah' => $this->input->post('lokasi_tanah'),
+                'luas_tanah' => $this->input->post('luas_tanah'),
+                'no_persil_tanah' => $this->input->post('no_persil_tanah'),
+                'tanah_dipakai' => $this->input->post('tanah_dipakai'),
+                'latitude' => $this->input->post('latitude'),
+                'longitude' => $this->input->post('longitude'),
 
-                // 'ktp_pemohon' => $this->input-post('ktp_pemohon'),
-                // 'akte_perusahaan' => $this->input-post('akte_perusahaan'),
-                // 'surat_perjanjian' => $this->input-post('surat_perjanjian'),
-                // 'jumlah_titik_menara' => $this->input-post('jumlah_titik_menara'),
-                // 'kontruksi_menara' => $this->input-post('kontruksi_menara'),
-                // 'sp_fo_ducting' => $this->input-post('sp_fo_ducting'),
-                // 'gambar_site' => $this->input-post('gambar_site'),
-                // 'sp_menara_bersama' => $this->input-post('sp_menara_bersama'),
+                'ktp_pemohon' => $ktp_pemohon,
+                'akte_perusahaan' => $akte_perusahaan,
+                'surat_perjanjian' => $surat_perjanjian,
+                'jumlah_titik_menara' => $jumlah_titik_menara,
+                'kontruksi_menara' => $kontruksi_menara,
+                'sp_fo_ducting' => $sp_fo_ducting,
+                'gambar_site' => $gambar_site,
+                'sp_menara_bersama' => $sp_menara_bersama,
             );
             $this->m_pendaftaran->Add($data);
-            redirect('peta-menara','refresh');
+            redirect('','refresh');
         }
+        
     }
-
     
 }
 
